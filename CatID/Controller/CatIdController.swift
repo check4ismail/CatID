450881bf73d735b371e1674aae7f6e12c3b57bf2
Ismail Elmaliki <check4elmaliki@live.com>
2019-10-03 10:10:58 -0400

    Created new view controller in place of existing CatIdController; implemented search bar in CatIdController

--- CatID/Controller/CatIdController.swift
+++ CatID/Controller/CatIdController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class CatIdController: UITableViewController {
+class CatIdController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
-	// Outlets
-	@IBOutlet weak var catBreedText: UITextField!
-	
+	// Outlets	
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var tableView: UITableView!
 	// Members
 	private let catBreeds: [String] = CatBreeds().getBreeds()
 	private var catSectionTitles = [String]()
 	private var catBreedDictionary = [String: [String]]()
+	private var searchActive: Bool = false
+	var filtered:[String] = []
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -37,11 +39,16 @@ class CatIdController: UITableViewController {
 		
 		catSectionTitles = [String](catBreedDictionary.keys)
 		catSectionTitles = catSectionTitles.sorted(by: { $0 < $1 })
+		
+		searchBar.delegate = self
+		tableView.delegate = self
+		tableView.dataSource = self
+		
 		tableView.reloadData()
 	}
 
 	//MARK: Segue to Cat Breed Controller
-	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		
 		performSegue(withIdentifier: "goToCatBreed", sender: self)
 	}
@@ -57,40 +64,101 @@ class CatIdController: UITableViewController {
 	//MARK: TableView methods
 	
 	// Number of sections
-	override func numberOfSections(in tableView: UITableView) -> Int {
-		return catSectionTitles.count
+	func numberOfSections(in tableView: UITableView) -> Int {
+		if searchActive {
+			return 1
+		} else {
+			return catSectionTitles.count
+		}
 	}
 	
 	// Number of rows in each indexed section
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let catKey = catSectionTitles[section]
-		if let catValues = catBreedDictionary[catKey] {
-			return catValues.count
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+		if searchActive {
+			return filtered.count
+		} else {
+			let catKey = catSectionTitles[section]
+			if let catValues = catBreedDictionary[catKey] {
+				return catValues.count
+			}
 		}
 			
 		return 0
     }
 	
 	// Populating each row in each section
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
 		
-		let catKey = catSectionTitles[indexPath.section]
-		if let catValues = catBreedDictionary[catKey] {
-			cell.textLabel?.text = catValues[indexPath.row]
+		if searchActive {
+			cell.textLabel?.text = filtered[indexPath.row]
+			print(cell)
+		} else {
+			let catKey = catSectionTitles[indexPath.section]
+			if let catValues = catBreedDictionary[catKey] {
+				cell.textLabel?.text = catValues[indexPath.row]
+			}
 		}
 		
 		return cell
 	}
 	
 	// Display header title
-	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		if searchActive {
+			return nil
+		}
 		return catSectionTitles[section]
 	}
 	
 	// Indexed table view displayed
-	override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+	func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+		if searchActive {
+			return nil
+		}
 		return catSectionTitles
+	}
+	
+	//MARK: search bar functions
+	
+	func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+		searchActive = true
+	}
+	
+	func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+		searchActive = false
+	}
+	
+	func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+		searchActive = false
+	}
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		searchActive = false
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		filtered = catBreeds.filter({ (text) -> Bool in
+			let tmp: NSString = NSString(string: text)
+			let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+		})
+		if(filtered.count == 0){
+			isSearchBarEmpty()
+        } else {
+            searchActive = true;
+        }
+		
+        tableView.reloadData()
+	}
+	
+	func isSearchBarEmpty() {
+		if searchBar.text != "" {
+			searchActive = true
+		} else {
+			searchActive = false
+		}
 	}
 }
 
