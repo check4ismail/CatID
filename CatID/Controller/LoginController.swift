1c08659bba95425a2c52d6b8330d67defa969b70
Ismail Elmaliki <check4elmaliki@live.com>
2020-01-01 16:46:10 -0500

    Built UI for login screen

---
+++ CatID/Controller/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginController.swift
+//  CatID
+//
+//  Created by Ismail Elmaliki on 1/1/20.
+//  Copyright © 2020 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+	
+	
+	override func viewDidLoad() {
+		
+	}
+	
+	@IBAction func attemptLogin(_ sender: UIButton) {
+		
+	}
+}
