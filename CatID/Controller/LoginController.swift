ae2991cf78cec21c8deeadd62b546e9b86a45b30
Ismail Elmaliki <check4elmaliki@live.com>
2020-01-14 07:17:38 -0500

    Setup UI for sign in and sign up screen - email and password only

--- CatID/Controller/LoginController.swift
+++ CatID/Controller/LoginController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class LoginController: UIViewController {
 	
+	@IBOutlet weak var signUpButton: UIButton!
+	
+	let segue = "signUpSegue"
 	
 	override func viewDidLoad() {
+		loadSignUp()
+	}
+	
+	private func loadSignUp() {
+		let newUserText = "New user?"
+		let signUpText = " Signup"
 		
+		let userAttribute = [NSAttributedString.Key.font: UIFont(name: "SFProRounded-Regular", size: 22.0)!,
+							  NSAttributedString.Key.foregroundColor: UIColor.black]
+		let signUpAttribute = [NSAttributedString.Key.font: UIFont(name: "SFProRounded-Semibold", size: 22.0)!,
+		NSAttributedString.Key.foregroundColor: UIColor.black]
+		
+		let attributedString = NSMutableAttributedString(string: newUserText, attributes: userAttribute)
+		attributedString.append(NSMutableAttributedString(string: signUpText, attributes: signUpAttribute))
+		
+		signUpButton.setAttributedTitle(attributedString, for: .normal)
 	}
 	
 	@IBAction func attemptLogin(_ sender: UIButton) {
-		
+		print("Touched button")
 	}
+	
+	@IBAction func signUpFlow(_ sender: UIButton) {
+		performSegue(withIdentifier: segue, sender: self)
+	}
+	
+	
 }
---
+++ CatID/Controller/SignUpController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpController.swift
+//  CatID
+//
+//  Created by Ismail Elmaliki on 1/2/20.
+//  Copyright © 2020 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+public class SignUpController: UIViewController {
+	
+	@IBAction func backToLogin(_ sender: UIButton) {
+		self.dismiss(animated: true, completion: nil)
+	}
+}
